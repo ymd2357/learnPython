471c6d4bb09b9143f7e4807ebca2ca7815b184cd
ky2357 <akandy.1ame@gmail.com>
2020-05-14 07:20:52 +0000

    String upper/lower/etc

--- StringOperation.py
+++ StringOperation.py
@@ -4,3 +4,12 @@ abc2 = ",".join(["Andy", "Bill", "Cody"])
 
 print(abc1)
 print(abc2)
+print("\n")
+
+print(abc2.lower())
+print(abc2.upper())
+print(abc2.capitalize())
+print(abc2.upper().title())
+print(abc2.swapcase())
+print("\n")
+
